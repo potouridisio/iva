e4a5b1a997b394cab345904cdabe2d412c5b168e
Ioannis Potouridis <potouridisio@gmail.com>
2023-10-16 08:12:32 +0300

    chore: add Profile type

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,33 @@ export type Message = {
   content: string
   role: 'assistant' | 'system' | 'user'
 }
+
+type Education = {
+  degreeName: string
+  endMonthYear: string
+  fieldOfStudyName: string
+  schoolName: string
+  startMonthYear: string
+}
+
+type Position = {
+  companyName: string
+  description: string
+  endMonthYear: string
+  startMonthYear: string
+  title: string
+}
+
+type Skill = {
+  name: string
+}
+
+export type Profile = {
+  educations: Education[]
+  firstName: string
+  headline: string
+  lastName: string
+  positions: Position[]
+  skills: Skill[]
+  summary: string
+}
